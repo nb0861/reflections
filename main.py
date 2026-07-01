1c549eb28a96ad095af1de42531493cddf38af9d
unknown <nbold0861@gmail.com>
2026-07-01 17:42:47 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def main() -> None:
+    date = input("Enter date (MM/DD/YYYY): ")
+    entry = input("Enter thoughts for the day: ")
+    print(f"{date}: {entry}")
+    
+if __name__ == "__main__":
+    main()
