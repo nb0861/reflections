b39b68fddcd83cdf3452713e13084e4654d9ea78
unknown <nbold0861@gmail.com>
2026-07-02 14:40:19 -0400

    create a Entry class to model the input

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+from dataclasses import dataclass
+
+@dataclass
+class Entry:
+    id: int
+    date: str
+    text: str
+
 def main() -> None:
-    date = input("Enter date (MM/DD/YYYY): ")
-    entry = input("Enter thoughts for the day: ")
-    print(f"{date}: {entry}")
-    
+    id = 0
+    while True:
+        date = input("Enter date (MM/DD/YYYY): ")
+        entry = input("Enter thoughts for the day: ")
+        entry = Entry(id=id, date=date, text=entry)
+        print(f"{date}: {entry}")
+        id += 1
+
 if __name__ == "__main__":
     main()
