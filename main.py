cf38eb2727d962326c8906c12a2a36120d5e3f8e
unknown <nbold0861@gmail.com>
2026-07-05 21:28:54 -0400

    gracefully exit while loop when user is done

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ class Entry:
 def main() -> None:
     id = 0
     while True:
-        date = input("Enter date (MM/DD/YYYY): ")
-        entry = input("Enter thoughts for the day: ")
-        entry = Entry(id=id, date=date, text=entry)
+        date = input("Enter date (MM/DD/YYYY, or press Enter to finish): ").strip()
+        if not date or date.lower() in {"quit", "q", "done"}:
+            break
+
+        text = input("Enter thoughts for the day: ")
+        entry = Entry(id=id, date=date, text=text)
         print(f"{date}: {entry}")
         id += 1
 
